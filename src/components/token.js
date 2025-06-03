601d230fec199a4dfb313ea8e6ad3528fd4186a1
100RAB <115261525+100rabx23@users.noreply.github.com>
2025-06-03 18:40:56 +0530

    Update token.js

--- src/components/token.js
+++ src/components/token.js
@@ -1,1 +1,16 @@
 
+// src/constants/token.js
+export const TOKEN_ADDRESS = "0xd2C46206C95520a60b83A2BD0e480FC51dBA75d3";
+
+export const TOKEN_ABI = [
+  {
+    "inputs": [
+      { "internalType": "address", "name": "recipient", "type": "address" },
+      { "internalType": "uint256", "name": "amount", "type": "uint256" }
+    ],
+    "name": "transfer",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
