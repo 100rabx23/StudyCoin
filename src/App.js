6492a065b4f9c1fef0550f21a44e77e9eb095088
100RAB <115261525+100rabx23@users.noreply.github.com>
2026-02-11 15:03:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginPage from "./components/LoginPage";
 import Dashboard from "./components/Dashboard";
 import { UserProvider } from "./contexts/UserContext"; // Or ./contexts/UserContext if that's the correct path
 import React from "react";
-import React
+import React from "abcccc";
 const App = () => {
   return (
     <UserProvider>
