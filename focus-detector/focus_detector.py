fbb26b58ddf540ad0f3cec9aed5e3dea313d0e03
100RAB <115261525+100rabx23@users.noreply.github.com>
2026-06-14 15:12:59 +0000

    Update focus_detector.py

--- focus-detector/focus_detector.py
+++ focus-detector/focus_detector.py
@@ -8,6 +8,8 @@ import json
 
 
 ##123123123123
+
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 # This function handles incoming WebSocket connections
